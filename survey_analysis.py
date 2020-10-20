4e8a780969642bcc4755efbc1df2900e7a120cf8
MellowLeopard <37899770+MellowLeopard@users.noreply.github.com>
2020-10-20 18:14:23 +0200

    Paired sample ttest analysis with plot.

--- survey_analysis.py
+++ survey_analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
 
 
 class SurveyRun:
@@ -13,6 +15,19 @@ class SurveyRun:
         self.confidence = confidence
         self.intuitiveness = intuitiveness
     
+    def score(self, question):
+        if question in self.__dict__:
+            return self.__dict__[question]
+        raise NotImplementedError
+        
+    def adjusted_score(self, question):
+        if question in self.__dict__:
+            if question in ("frequency", "ease", "learnability", "confidence"):
+                return self.__dict__[question]
+            else:
+                return 6 - self.__dict__[question]
+        raise NotImplementedError
+    
     def __str__(self):
         return "{" + ', '.join(f"\"{str(k)}\": {str(v)}" for k, v in self.__dict__.items()) + "}"
 
@@ -25,6 +40,17 @@ class SurveyEntry:
         self.remarks1 = row[18]
         self.remarks2 = row[19]
     
+    def get_group(self, with_implicit_confirmation):
+        if self.group == "a" and with_implicit_confirmation:
+            return self.second
+        elif self.group == "a" and not with_implicit_confirmation:
+            return self.first
+        elif self.group == "b" and with_implicit_confirmation:
+            return self.first
+        elif self.group == "b" and not with_implicit_confirmation:
+            return self.second
+        raise NotImplementedError()
+        
     def __str__(self):
         return f"{{\"group\": \"{self.group}\", \"first\": {self.first}, \"second\": {self.second}, " \
                f"\"remarks1\": \"{self.remarks1}\", \"remarks2\": \"{self.remarks2}\"}}"
@@ -47,9 +73,43 @@ class Survey:
         return f"{{\"filename\": \"{self.filename}\", \"entries\": [{', '.join(str(e) for e in self.entries)}]}}"
 
 
-def main():
+def paired_sample_ttest(save=False):
     survey = Survey("participant_survey.csv")
-    print(survey)
+    data = {"total: satisfaction": ([], [])}
+    for participant in survey.entries:
+        for i, condrun in enumerate((participant.get_group(True), participant.get_group(False))):
+            satisfaction = 0
+            for j, question in enumerate(condrun.__dict__):
+                name = f"q{j + 1}: {question}"
+                if name not in data:
+                    data[name] = ([], [])
+                data[name][i].append(condrun.score(question))
+                satisfaction += condrun.adjusted_score(question)
+            data["total: satisfaction"][i].append(satisfaction / len(condrun.__dict__))
+    ttests = {}
+    for category in data:
+        with_conf, without_conf = data[category]
+        ttests[category] = stats.ttest_rel(without_conf, with_conf)
+    categories = [c for c in ttests]
+    ticks = [i for i in range(len(categories))]
+    fig, axes = plt.subplots(2, 1, sharex="all")
+    axes[0].bar(ticks, [ttests[c][0] for c in categories])
+    axes[1].bar(ticks, [ttests[c][1] for c in categories], color="orange")
+    plt.xticks(ticks, categories)
+    axes[1].set_xticklabels(categories, rotation=45)
+    axes[1].hlines(0.05, -0.5, len(categories) - 0.5, alpha=0.2)
+    axes[0].tick_params(axis="both", which="both", labelsize=8)
+    axes[1].tick_params(axis="both", which="both", labelsize=8)
+    axes[0].set_title("Paired T-test statistics", fontsize=8)
+    axes[1].set_title("Paired T-test p-values", fontsize=8)
+    fig.tight_layout()
+    if save:
+        fig.savefig(f"paired_sample_ttest_results", dpi=150)
+    print(data)
+
+
+def main():
+    paired_sample_ttest(True)
 
 
 if __name__ == "__main__":
